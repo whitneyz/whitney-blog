d016785be6642266312a66c2df288a6a6d8df5ca
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2021-01-07 09:38:12 +0100

    added single component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 import './App.css';
 
 function App() {
+    //you can output strings, numbers (int), arrays but not booleans and objects
+    const title = "Whitney's blog";
   return (
     <div className="App">
       <div className="content">
-          <h1>App Component</h1>
+          <h1>{ title }</h1>
+          {/*you can also output directly in the tags, see the exp below*/}
+          {/*this is how you add comments in react jsx*/}
+          <p>{"Welcome to my blog"}</p>
 
       </div>
     </div>
