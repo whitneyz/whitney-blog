b5f4f74e089dbe40f4ca385d9a9be1c0bc3cadb7
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2021-01-08 11:52:50 +0100

    excersize with dynamic nam value onClick and show onscreen. I will delete that hereafter to continue the course

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {useState} from 'react';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+
+    // let name = 'given';
+    // useState can be any datatype (array, string, boolean...)
+    const [name, setName] = useState('given');
+    const [age,setAge] = useState('10');
+
+    const handleClick = () => {
+        setName('mimi');
+        setAge('12');
+    }
     return (
         <div className="Home">
-            <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 };
