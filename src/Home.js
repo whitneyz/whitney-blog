099fd31953f4da6df93f32a4bd46d9f4c400eef4
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2021-01-14 11:10:01 +0100

    fetched data with json

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,7 @@ import {useState, useEffect} from 'react';
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     // useState can be any datatype (array, string, boolean...)
     const handleDelete = (id) => {
@@ -15,12 +11,19 @@ const Home = () => {
         setBlogs(newBlogs);
     }
     useEffect(() => {
-
-    });
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                {/*console.log(data);*/}
+                setBlogs(data);
+            });
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
         </div>
     );
 };
